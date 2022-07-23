1ece05ed365ff22ef353210f043182f0b2b374af
Vu Bui Khanh Linh <73817943+Linh-0v0@users.noreply.github.com>
2022-07-23 09:35:33 +0700

    Initial Commit

---
+++ ASM1/ASM1App.swift
@@ -0,0 +1,17 @@
+//
+//  ASM1App.swift
+//  ASM1
+//
+//  Created by Vu Bui Khanh Linh on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ASM1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASM1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ASM1
+//
+//  Created by Vu Bui Khanh Linh on 23/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
